9adcfde9be3f4d94fc502ab96cdd6986a8687697
LChumi <112025391+LChumi@users.noreply.github.com>
2025-04-22 21:21:26 -0500

    feat:added swagger dependency and added openapidefinition

--- src/main/java/com/alquimiasoft/minegocio/MinegocioApplication.java
+++ src/main/java/com/alquimiasoft/minegocio/MinegocioApplication.java
@@ -1,9 +1,12 @@
 package com.alquimiasoft.minegocio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Minegocio", version = "1.0.0"))
 public class MinegocioApplication {
 
 	public static void main(String[] args) {
